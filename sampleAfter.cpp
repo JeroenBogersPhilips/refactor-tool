986a3f5d5042f19d64eb59e4d4faa32345e88ca6
Jeroen Bogers <33284848+JeroenBogersPhilips@users.noreply.github.com>
2017-11-07 15:57:47 +0100

    ran clang-format

--- sampleAfter.cpp
+++ sampleAfter.cpp
@@ -1,56 +1,40 @@
-struct X_aa
-{
-   X_aa(int a, int b) {}
 
-   ~X_aa() {}
+struct X_aa {
+  X_aa(int a, int b) {}
 
-   X_aa(const X_aa&);
+  ~X_aa() {}
 
-   X_aa(X_aa&&);
+  X_aa(const X_aa &);
 
-   static void printUnderscores() {}
+  X_aa(X_aa &&);
+
+  static void printUnderscores() {}
 };
 
-void f2()
-{
+void f2() {}
 
+template <typename T> void functionWithUnderscores(const T &) {
+  f2();
+  X_aa::printUnderscores();
 }
 
-template <typename T>
-void functionWithUnderscores(const T&)
-{
-   f2();
-   X_aa::printUnderscores();
-}
+template <typename T> class X;
 
-template <typename T>
-class X;
-
-template <typename T>
-class X<T*>
-{
+template <typename T> class X<T *> {
 public:
-    void someMethodWithUnderscores()
-    {
-    	someMethodWithUnderscoresImpl();
-    }
+  void someMethodWithUnderscores() { someMethodWithUnderscoresImpl(); }
 
-	void someMethodWithUnderscoresImpl()
-	{
-	}
+  void someMethodWithUnderscoresImpl() {}
 };
 
-template <>
-class X<int>
-{
+template <> class X<int> {
 public:
-	void someOtherMethod();
+  void someOtherMethod();
 };
 
-int main()
-{
-    X<int*> x;
-    x.someMethodWithUnderscores();
+int main() {
+  X<int *> x;
+  x.someMethodWithUnderscores();
 
-	return 0;
+  return 0;
 }
